567ac6e6043cb0e38a5e55214a011dcd52532298
kunj3740 <kunjdave694@gmail.com>
2024-06-17 23:25:38 +0530

    first commit done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signup } from './pages/Signup'
+import { Signin } from './pages/Signin'
+import { Blog } from './pages/Blog'
+import { Blogs } from './pages/Blogs'
+import { Publish } from './pages/Publish'
+import Homepage from './pages/HomePage'
+import Myblogs from './pages/MyBlogs'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/publish" element={<Publish />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/myblogs" element={<Myblogs />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { Appbar } from "../components/Appbar"
+import { BlogCard } from "../components/BlogCard"
+import { BlogSkeleton } from "../components/BlogSkeleton";
+import { Blog, useBlogs } from "../hooks";
+
+export const Blogs = () => {
+    const { loading, blogs } = useBlogs();
+    const [searchTerm, setSearchTerm] = useState("");
+    const [suggestion, setSuggestion] = useState<Blog[]>([]);
+    useEffect(() => {
+        if (!searchTerm) {
+            setSuggestion(blogs);
+        } else {
+            const results = blogs.filter((blog) =>
+                blog.title.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+            setSuggestion(results);
+        }
+    }, [searchTerm, blogs]);
+    const handleSearchChange = (event:any) => {
+        setSearchTerm(event.target.value);
+    };
+    if (loading) {
+        return <div>
+            <Appbar /> 
+            <div  className="flex justify-center">
+                <div>
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                </div>
+            </div>
+        </div>
+    }
+
+    return <div>
+    <Appbar />
+    <div className="flex justify-center my-4 mt-5">
+        <input
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            className="border rounded py-2 px-4"
+            placeholder="Find your blog..."
+        />
+    </div>
+    <div className="flex justify-center">
+        <div>
+            {(searchTerm ? suggestion : blogs).map((blog) => (
+                <BlogCard
+                    key={blog.id}
+                    id={blog.id}
+                    authorName={blog.author.name || "Anonymous"}
+                    title={blog.title}
+                    content={blog.content}
+                    publishedDate={"2nd Feb 2024"}
+                />
+            ))}
+        </div>
+    </div>
+</div>
+}
