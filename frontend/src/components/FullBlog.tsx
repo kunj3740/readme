8e5b305dfab92e5179278170f46b2128efdc3028
kunj3740 <kunjdave694@gmail.com>
2024-08-17 14:33:51 +0530

    bug fixed

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -64,9 +64,9 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
     return <div>
         <Appbar />
         <div className="flex justify-center">
-            <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
+            <div className="grid grid-cols-12 px-5 w-full max-w-screen-xl pt-8">
                 <div className="col-span-12 md:col-span-8">
-                    <div className="text-5xl font-extrabold">
+                    <div className="text-3xl md:text-5xl font-extrabold">
                         {blog.title}
                     </div>
                     <div className="text-slate-500 pt-2">
@@ -76,7 +76,7 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
                         {blog.content}
                     </div>
                 </div>
-                <div className="md:col-span-4 md:ml-16">
+                <div className="md:col-span-4 hidden md:ml-16">
                     <div className="text-slate-600 text-lg">
                         Author
                     </div>
