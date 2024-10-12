fee10b23e4755be4d49ade729254ff7c29a0cd3f
kunj3740 <kunjdave694@gmail.com>
2024-10-13 01:45:14 +0530

    rag testing

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = "https://backend.kunjdave694.workers.dev"
-export const RAG_URL="http://127.0.0.1:8787"
-// export const RAG_URL="https://rag.kunjdave694.workers.dev"
+// export const RAG_URL="http://127.0.0.1:8787"
+ export const RAG_URL="https://rag.kunjdave694.workers.dev"
